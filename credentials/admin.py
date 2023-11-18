eb9ec04ab32b92446de02b9e8955e8c1c9e96e91
Nida07 <nidasheraf@gmail.com>
2023-11-18 07:01:39 +0530

    the time schedule base updated

--- credentials/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import biometric
-
-# Register your models here.
-admin.site.register(biometric)
--- credentials/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import biometric
-
-class BiometricForm(forms.ModelForm):
-    class Meta:
-        model = biometric
-        fields = ['height','weight','bloodpressure'] # This includes all fields in the form
